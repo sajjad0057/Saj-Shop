4d9ed14172ef325f1297539588d36687fd1404eb
sajjad0057 <sajjadhossain0057@gmail.com>
2021-04-25 03:02:25 +0600

    rendering (basic) JsonResponse

---
+++ backend/base/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from .products import products
+
+# Create your views here.
+
+
+def getRoutes(request):
+    return JsonResponse('Hello',safe=False)
+
+
+def getProducts(request):
+    return JsonResponse(products,safe=False)
