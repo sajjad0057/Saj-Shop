2415bf99ecf418860c29164e1e565d8c49e4509d
sajjad0057 <sajjadhossain0057@gmail.com>
2021-04-25 05:05:03 +0600

    Basic DRF functionality (rendering Response)

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .products import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
 
 # Create your views here.
 
-
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello',safe=False)
+    routes = [
+        "api/products/",
+        "api/products/create/",
+        "api/products/upload/",
+        "api/products/<id>/reviews/",
+        "api/products/top/",
+        "api/products/<id>/",
+        "api/products/delete/<id>/",
+        "api/products/update/<id>/",
+    ]
+    return Response(routes)
 
-
+@api_view(['GET'])
 def getProducts(request):
-    return JsonResponse(products,safe=False)
+    return Response(products)
+
+
+@api_view(['GET'])
+def getSingleProduct(request,pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+        else:
+            product = {'message':'something wrong !'}
+    return Response(product)
