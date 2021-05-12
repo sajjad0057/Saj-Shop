1e8f08a7ba3abdbbbf7ef6ab8f51dd6d982f7386
sajjad0057 <sajjadhossain0057@gmail.com>
2021-05-13 01:58:11 +0600

    JWT Customizations

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -10,5 +10,5 @@ urlpatterns= [
     path('', views.getRoutes, name="routes"),
     path('products/',views.getProducts,name="products"),
     path('products/<str:pk>/',views.getSingleProduct,name="product"),
-    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/login/', views.MyTokenObtainPairView.as_view(), name='MyTokenObtainPairView'),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -6,6 +6,23 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+# For simple-jwt
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+        return data
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
 
 
 # Create your views here.
