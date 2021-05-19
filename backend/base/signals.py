bb30f7a1c414aa0a509a1c88815968db5295a9ce
sajjad0057 <sajjadhossain0057@gmail.com>
2021-05-20 02:53:58 +0600

    login with Email (handle by django signals)

--- backend/base/signals.py
+++ backend/base/signals.py
@@ -4,7 +4,10 @@ from django.contrib.auth.models import User
 
 
 def updateUser(sender,instance,**kwargs):
-    print('base signals Triggered/fired')
+    # print('base signals Triggered/fired')
+    user = instance
+    if user.email != '':
+        user.username = user.email
 
 
 pre_save.connect(updateUser,sender=User)
